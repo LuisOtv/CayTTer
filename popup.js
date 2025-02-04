48c1dc94d22f70424833986c3a8c33aeef07480e
LuisOtv <luispim2005@gmail.com>
2025-02-04 17:50:58 -0300

    add side bar toggles

--- popup.js
+++ popup.js
@@ -1,36 +1,180 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const blue = document.getElementById("blueuser");
-	const square = document.getElementById("squared");
+  const blue = document.getElementById("blueuser");
+  const square = document.getElementById("squared");
+  const home = document.getElementById("home");
+  const search = document.getElementById("search");
+  const notification = document.getElementById("notifications");
+  const dms = document.getElementById("dms");
+  const grok = document.getElementById("grok");
+  const bookmark = document.getElementById("bookmark");
+  const communities = document.getElementById("community");
+  const premium = document.getElementById("premium");
+  const verified = document.getElementById("verified");
+  const notes = document.getElementById("notes");
+  const profile = document.getElementById("profile");
+  const more = document.getElementById("more");
 
-	// verify if twitter is open
-	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-		if (!tabs.length || !tabs[0].url || !tabs[0].url.includes("x.com")) {
-			return;
-		}
+  // verify if twitter is open
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (!tabs.length || !tabs[0].url || !tabs[0].url.includes("x.com")) {
+      return;
+    }
 
-		// Load state
-		chrome.storage.sync.get("hideBlueUsers", (data) => {
-			blue.checked = data.hideBlueUsers || false;
-		});
-		chrome.storage.sync.get("squareImages", (data) => {
-			square.checked = data.squareImages || false;
-		});
+    // Load state
+    chrome.storage.sync.get("hideBlueUsers", (data) => {
+      blue.checked = data.hideBlueUsers || false;
+    });
+    chrome.storage.sync.get("squareImages", (data) => {
+      square.checked = data.squareImages || false;
+    });
+    chrome.storage.sync.get("hideHome", (data) => {
+      home.checked = data.hideHome || false;
+    });
+    chrome.storage.sync.get("hideSearch", (data) => {
+      search.checked = data.hideSearch || false;
+    });
+    chrome.storage.sync.get("hideDMs", (data) => {
+      search.checked = data.hideSearch || false;
+    });
+    chrome.storage.sync.get("hideNotification", (data) => {
+      notification.checked = data.hideNotification || false;
+    });
+    chrome.storage.sync.get("hideGrok", (data) => {
+      grok.checked = data.hideGrok || false;
+    });
+    chrome.storage.sync.get("hideBookmark", (data) => {
+      bookmark.checked = data.hideBookmark || false;
+    });
+    chrome.storage.sync.get("hideCommunity", (data) => {
+      communities.checked = data.hideCommunity || false;
+    });
+    chrome.storage.sync.get("hidePremium", (data) => {
+      premium.checked = data.hidePremium || false;
+    });
+    chrome.storage.sync.get("hideVerified", (data) => {
+      verified.checked = data.hideVerified || false;
+    });
+    chrome.storage.sync.get("hideNotes", (data) => {
+      notes.checked = data.hideNotes || false;
+    });
+    chrome.storage.sync.get("hideProfile", (data) => {
+      profile.checked = data.hideProfile || false;
+    });
+    chrome.storage.sync.get("hideMore", (data) => {
+      more.checked = data.hideMore || false;
+    });
 
-		// Event listeners for checkboxes
-		blue.addEventListener("change", function () {
-			chrome.storage.sync.set({ hideBlueUsers: blue.checked });
-			chrome.tabs.sendMessage(tabs[0].id, {
-				action: "toggleBlueUsers",
-				hide: blue.checked,
-			});
-		});
+    // Event listeners for checkboxes
+    blue.addEventListener("change", function () {
+      chrome.storage.sync.set({ hideBlueUsers: blue.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleBlueUsers",
+        hide: blue.checked,
+      });
+    });
 
-		square.addEventListener("change", function () {
-			chrome.storage.sync.set({ squareImages: square.checked });
-			chrome.tabs.sendMessage(tabs[0].id, {
-				action: "toggleSquareImages",
-				hide: square.checked,
-			});
-		});
-	});
+    square.addEventListener("change", function () {
+      chrome.storage.sync.set({ squareImages: square.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleSquareImages",
+        hide: square.checked,
+      });
+    });
+
+    home.addEventListener("change", function () {
+      chrome.storage.sync.set({ hideHome: home.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleHome",
+        hide: home.checked,
+      });
+    });
+
+    search.addEventListener("change", function () {
+      chrome.storage.sync.set({ hideSearch: search.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleSearch",
+        hide: search.checked,
+      });
+    });
+
+    notification.addEventListener("change", function () {
+      chrome.storage.sync.set({ hideNotification: notification.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleNotification",
+        hide: notification.checked,
+      });
+    });
+
+    dms.addEventListener("change", function () {
+      chrome.storage.sync.set({ hideSearch: dms.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleDMs",
+        hide: dms.checked,
+      });
+    });
+
+    grok.addEventListener("change", function () {
+      chrome.storage.sync.set({ hideGrok: grok.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleGrok",
+        hide: grok.checked,
+      });
+    });
+
+    bookmark.addEventListener("change", function () {
+      chrome.storage.sync.set({ hideGrok: bookmark.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleBookmark",
+        hide: bookmark.checked,
+      });
+    });
+
+    communities.addEventListener("change", function () {
+      chrome.storage.sync.set({ hideCommunity: communities.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleCommunity",
+        hide: communities.checked,
+      });
+    });
+
+    premium.addEventListener("change", function () {
+      chrome.storage.sync.set({ hidePremium: premium.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "togglePremium",
+        hide: premium.checked,
+      });
+    });
+
+    verified.addEventListener("change", function () {
+      chrome.storage.sync.set({ hideVerified: verified.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleVerified",
+        hide: verified.checked,
+      });
+    });
+
+    notes.addEventListener("change", function () {
+      chrome.storage.sync.set({ hideNotes: notes.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleNotes",
+        hide: notes.checked,
+      });
+    });
+
+    profile.addEventListener("change", function () {
+      chrome.storage.sync.set({ hideProfile: profile.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleProfile",
+        hide: profile.checked,
+      });
+    });
+
+    more.addEventListener("change", function () {
+      chrome.storage.sync.set({ hideMore: more.checked });
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "toggleMore",
+        hide: more.checked,
+      });
+    });
+  });
 });
--- settings.js
+++ settings.js
@@ -1,42 +1,170 @@
 chrome.runtime.onMessage.addListener((message) => {
-	if (message.action === "toggleBlueUsers") {
-		if (message.hide) {
-			document.styleSheets[0].insertRule(
-				'[data-testid="tweet"]:has([data-testid="icon-verified"]) { display: none !important; }',
-				0
-			);
-		} else {
-			Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
-				if (rule.cssText.includes('data-testid="icon-verified"')) {
-					document.styleSheets[0].deleteRule(index);
-				}
-			});
-		}
-	} else if (message.action === "toggleSquareImages") {
-		if (message.hide) {
-			document.styleSheets[0].insertRule(
-				".r-sdzlij{border-radius: 0px !important}",
-				0
-			);
-		} else {
-			Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
-				if (rule.cssText.includes(".r-sdzlij")) {
-					document.styleSheets[0].deleteRule(index);
-				}
-			});
-		}
-	}
+  if (message.action === "toggleBlueUsers") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('[data-testid="tweet"]:has([data-testid="icon-verified"]) { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('data-testid="icon-verified"')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "toggleSquareImages") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule(".r-sdzlij{border-radius: 0px !important}", 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes(".r-sdzlij")) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "toggleHome") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('a[href*="/home"] { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('a[href*="/home"]')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "toggleSearch") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('a[href*="/explore"] { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('a[href*="/explore"]')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "toggleNotification") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('a[href="/notifications"] { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('a[href="/notifications"]')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "toggleDMs") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('a[href="/messages"] { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('a[href="/messages"]')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "toggleGrok") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('a[href="/i/grok"] { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('a[href="/i/grok"]')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "toggleBookmark") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('a[href="/i/bookmarks"] { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('a[href="/i/bookmarks"]')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "toggleCommunity") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('a[href*="/communities"] { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('a[href*="/communities"]')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "togglePremium") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('a[href="/i/premium_sign_up"] { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('a[href="/i/premium_sign_up"]')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "toggleVerified") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('a[href="/i/verified-orgs-signup"] { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('a[href="/i/verified-orgs-signup"]')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "toggleNotes") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('a[href="/i/communitynotes"] { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('a[href="/i/communitynotes"]')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "toggleProfile") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('a[href="/i/profile"] { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('a[href="/i/profile"]')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  } else if (message.action === "toggleMore") {
+    if (message.hide) {
+      document.styleSheets[0].insertRule('[data-testid="AppTabBar_More_Menu"] { display: none !important; }', 0);
+    } else {
+      Array.from(document.styleSheets[0].cssRules).forEach((rule, index) => {
+        if (rule.cssText.includes('[data-testid="AppTabBar_More_Menu"]')) {
+          document.styleSheets[0].deleteRule(index);
+        }
+      });
+    }
+  }
 });
-
-// load state on page open
+// Load state on page open
 chrome.storage.sync.get("hideBlueUsers", (data) => {
-	if (data.hideBlueUsers) {
-		document.styleSheets[0].insertRule(
-			'[data-testid="tweet"]:has([data-testid="icon-verified"]) { display: none !important; }',
-			0
-		);
-	}
-	if (data.squareImages) {
-		document.styleSheets[0].insertRule(".r-sdzlij{border-radius: 0px}", 0);
-	}
+  if (data.hideBlueUsers) {
+    document.styleSheets[0].insertRule('[data-testid="tweet"]:has([data-testid="icon-verified"]) { display: none !important; }', 0);
+  }
+  if (data.squareImages) {
+    document.styleSheets[0].insertRule(".r-sdzlij{border-radius: 0px}", 0);
+  }
+  if (data.hideHome) {
+    document.styleSheets[0].insertRule('a[href*="/explore"] { display: none !important; }', 0);
+  }
+  if (data.hideCommunity) {
+    document.styleSheets[0].insertRule('a[href*="/communities"] { display: none !important; }', 0);
+  }
+  if (data.hideCommunityNotes) {
+    document.styleSheets[0].insertRule('a[href="/i/communitynotes"] { display: none !important; }', 0);
+  }
+  if (data.hidePremium) {
+    document.styleSheets[0].insertRule('a[href="/i/premium_sign_up"] { display: none !important; }', 0);
+  }
+  if (data.hideGrok) {
+    document.styleSheets[0].insertRule('a[href="/i/grok"] { display: none !important; }', 0);
+  }
+  if (data.hideVerified) {
+    document.styleSheets[0].insertRule('a[href="/i/verified-orgs-signup"] { display: none !important; }', 0);
+  }
 });
